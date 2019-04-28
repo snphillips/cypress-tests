3ddb4f8a3d048237fd1abda003c5b1458972cf05
Sarah Phillips <snphillips@gmail.com>
2019-04-28 13:47:51 -0400

    so trashy refuse type test created

--- cypress/integration/sotrashy.spec.js
+++ cypress/integration/sotrashy.spec.js
@@ -6,7 +6,7 @@ context('Actions', () => {
     cy.visit('https://so-trashy-react.surge.sh/')
   })
 
-  it('selects year dropdown menu', () => {
+  it('selects all the year dropdown menu', () => {
     cy.get('select').select('2017')
     cy.get('select').select('2016')
     cy.get('select').select('2015')
@@ -15,6 +15,16 @@ context('Actions', () => {
     cy.get('select').select('2012')
     cy.get('select').select('2011')
     cy.get('select').select('2010')
+  })
+
+  it('clicks all the refuse type radio buttons', () => {
+    cy.get('#allcollected').click({force: true})
+    cy.get('#refusetonscollected').click({force: true})
+    cy.get('#papertonscollected').click({force: true})
+    cy.get('#mgptonscollected').click({force: true})
+    cy.get('#resorganicstons').click({force: true})
+    cy.get('#leavesorganictons').click({force: true})
+    cy.get('#xmastreetons').click({force: true})
   })
 
 })
